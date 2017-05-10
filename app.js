474aede80c193edf9415e0083611b992e3f129f9
Yitzi2 <ytkornbluth@hotmail.com>
2017-05-10 16:25:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// your code here!
+function uniqueSort (arr) {
+  let temp=arr.slice(0).sort();
+  let current=temp[0];
+  for (i=1; i<temp.length; ++i) {
+    if (temp[i]===current) temp[i]=null;
+    else current=temp[i];
+  }
+  return temp.filter(word=>word!==null);
+}
+
+$(".js-text-analysis-form").submit (function () {
+  $(".js-unhide-on-submit").removeClass("hidden");
+  let words=$(this).text().toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean);
+  $(".js-word-count").text(words.length);
+  $(".js-unique-words").text(words.uniqueSort.length);
+  return false;
+  }
+)
