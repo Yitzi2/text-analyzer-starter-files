8a511b6b8195af19eecb07af444bf73a36af6d99
Yitzi2 <ytkornbluth@hotmail.com>
2017-05-10 16:47:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-  console.log("a");
-
 function uniqueSort (arr) {
   let temp=arr.slice(0).sort();
   let current=temp[0];
@@ -10,12 +8,17 @@ function uniqueSort (arr) {
   return temp.filter(word=>word!==null);
 }
 
-$(".js-text-analysis-form").submit (function () {
-  $(".js-unhide-on-submit").removeClass("hidden");
-  let words=$(this).text().toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean);
-  console.log(words);
-  $(".js-word-count").text(words.length);
-  $(".js-unique-words").text(uniqueSort(words).length);
+$(".js-text-analysis-form").submit (
+    function () {
+    $(".js-unhide-on-submit").removeClass("hidden");
+    let words=$(".js-text-analysis-input").val().toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean);
+    $(".js-word-count").text(words.length);
+    $(".js-unique-words").text(uniqueSort(words).length);
+    let sumCounter=0;
+    for (i=0; i<words.length; ++i) {
+    sumCounter+=words[i].length;
+    }
+    $(".js-average-length").text(sumCounter/words.length);
   return false;
   }
 )
