58a66ce87e4ffb0f408054d7bc6e5a1f32f5e63a
Yitzi2 <ytkornbluth@hotmail.com>
2017-05-10 17:02:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,21 +8,17 @@ function uniqueSort (arr) {
   return temp.filter(word=>word!==null);
 }
 
-function redefineSubmit(){
-  $(".js-text-analysis-form").submit (
+$(".js-text-analysis-form").submit (
     function () {
-      $(".js-unhide-on-submit").removeClass("hidden");
-      let words=$(".js-text-analysis-input").val().toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean);
-      $(".js-word-count").text(words.length);
-      $(".js-unique-words").text(uniqueSort(words).length);
-      let sumCounter=0;
-      for (i=0; i<words.length; ++i) {
-        sumCounter+=words[i].length;
-      }
-      $(".js-average-length").text(sumCounter/words.length);
-    return false;
+    $(".js-unhide-on-submit").removeClass("hidden");
+    let words=$(".js-text-analysis-input").val().toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean);
+    $(".js-word-count").text(words.length);
+    $(".js-unique-words").text(uniqueSort(words).length);
+    let sumCounter=0;
+    for (i=0; i<words.length; ++i) {
+    sumCounter+=words[i].length;
     }
-  )
-}
-  
-$(redefineSubmit);
+    $(".js-average-length").text(sumCounter/words.length);
+  return false;
+  }
+)
